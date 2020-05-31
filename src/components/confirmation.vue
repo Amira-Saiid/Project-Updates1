16c806ea700c1b443f27eaf219a3f5b4574873db
Amira-Saiid <58108079+Amira-Saiid@users.noreply.github.com>
2020-05-31 22:43:09 +0200

    Add files via upload

--- src/components/confirmation.vue
+++ src/components/confirmation.vue
@@ -1,13 +1,14 @@
 <template>
  <div class="container-fluid text-center" style="height:100%" id="info" v-if="this.$store.state.newEmail">
-    <img src="../logo/white_notify.png" alt="" class="mx-auto d-block pt-5">
+    <img src="../logo/logotexttt.png" alt="" class="mx-auto d-block pt-5 mt-5">
+    <!-- <img src="../logo/white_notify.png" alt="" class="mx-auto d-block pt-5"> -->
 <div class="row">
   <div class="col-sm-4"></div>
   <div class="col-sm-4 mt-5">
     <h2 class="mt-5">Almost there...</h2>
     <p>Please check your email ({{this.$store.state.newEmail}}) <br> to confirm your account.</p>
     <hr>
-    <p class="mt-5 pt-3">If {{this.$store.state.newEmail}} is not your email address,<br> please <router-link  to="/register">go back</router-link> and enter the correct one.</p>
+    <p class="mt-5 pt-3">If <b>{{this.$store.state.newEmail}} </b>is not your email address,<br> please <router-link  to="/register">go back</router-link> and enter the correct one.</p>
     <p class="mt-5 pt-3 ">If you haven't received our email in 15 minutes,<br> please check your spam folder.</p>
   </div>
   <div class="col-sm-4"></div>
@@ -23,9 +24,16 @@ export default {
 </script>
 
 <style scoped>
-#info{
-  background-color: #164e87;
-  color: white;
-}
-</style>
 
+#info{
+   background-image: url('../logo/backbanner.png');
+  background-size: cover;
+  background-repeat: round;
+}
+
+#text{
+position: relative;
+/* margin-bottom: -4%; */
+
+}
+</style> 
